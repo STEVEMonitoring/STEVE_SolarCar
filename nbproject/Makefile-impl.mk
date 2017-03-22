ced066af04927d7daacf576d66d8d0f5c318b669
Fredrik Carlsson <fredrik.carlsson.92@gmail.com>
2017-03-22 17:31:20 +0100

    Initial commit of the project using the PIC18F26K80 processor

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=SolarCarMonitoringSystem.X
+PROJECTNAME=Solar Car
 
 # Active Configuration
 DEFAULTCONF=default
