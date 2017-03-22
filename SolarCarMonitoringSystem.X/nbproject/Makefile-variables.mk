1ebbb1544282318f73c790eeea0e1d977a5300e9
Fredrik Carlsson <fredrik.carlsson.92@gmail.com>
2017-03-22 17:10:25 +0100

    Initial commit for the project

---
+++ SolarCarMonitoringSystem.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SolarCarMonitoringSystem.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SolarCarMonitoringSystem.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=solarcarmonitoringsystem.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/solarcarmonitoringsystem.x.tar
