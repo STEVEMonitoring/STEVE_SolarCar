ee652a442ab3e3de1be9a117964f82b68351bf8c
Fredrik Carlsson <fredrik.carlsson.92@gmail.com>
2017-03-29 01:54:29 +0200

    Changed timer to loopback every 10ms insted of 100ms

--- STEVE_SolarCar/Src/main.c
+++ STEVE_SolarCar/Src/main.c
@@ -232,7 +232,7 @@ static void MX_TIM6_Init(void)
   htim6.Instance = TIM6;
   htim6.Init.Prescaler = 4800;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 1000;
+  htim6.Init.Period = 100;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
     Error_Handler();
