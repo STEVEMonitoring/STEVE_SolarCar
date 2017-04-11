a7c9ddce08b0fe09af5b9cdf379a81702996db3d
Fredrik Carlsson <fredrik.carlsson.92@gmail.com>
2017-04-11 09:12:11 +0200

    Added the last frames for the CAN communication and also added the receive and transmit id:s as structs

--- STEVE_SolarCar/Inc/dataStructs.h
+++ STEVE_SolarCar/Inc/dataStructs.h
@@ -8,6 +8,26 @@
 #ifndef DATASTRUCTS_H_
 #define DATASTRUCTS_H_
 
+struct CANidSend
+{
+	u_int16_t MPPT = 0x719;
+	u_int16_t BMS = 0x180;
+	u_int64_t motorController = 0x08F91540;
+
+
+
+};
+
+struct CANidReceive
+{
+	u_int16_t MPPT = 0x779;
+	u_int16_t BMSPDO1 = 0x200;
+	u_int16_t BMSPDO2 = 0x300;
+	u_int16_t BMSPDO3 =	0x400
+	u_int64_t motorFrame0 = 0x08850245;
+	u_int64_t motorFrame1 = 0x08950245;
+	u_int64_t motorFrame2 = 0x08A50245
+};
 
 
 struct PDO1
@@ -75,7 +95,35 @@ struct MotorFrame1
 
 struct MotorFrame2
 {
-	//todo
+	/**
+	 * @Analog Sensor Error 				bit 0
+	 * Motor Current Sensor U Error			bit 1
+	 * Motor Current Sensor W Error			bit 2
+	 * FET Thermistor Error					bit 3
+	 * TBD									bit 4
+	 * Battery Voltage Sensor Error			bit 5
+	 * Battery Current Sensor Error			bit 6
+	 * Battery Current Sensor Adjust Error	bit 7
+	 * Motor Current Sensor Adjust Error	bit 8
+	 * Accelerator Position Error			bit 9
+	 * TBD									bit 10
+	 * Controller Voltage Sensor Error		bit 11
+	 * TBD									bit 12-15
+	 * Power System Error					bit 16
+	 * Over Current Error					bit 17
+	 * TBD									bit 18
+	 * Over Voltage Error					bit 19
+	 * TBD									bit 20
+	 * Over Current Limit					bit 21
+	 * TBD									bit 22-23
+	 * Motor System Error					bit 24
+	 * Motor Lock							bit 25
+	 * Hall Sensor Short					bit 26
+	 * Hall Sensor Open						bit 27
+	 * TBD									bit 28-31
+	 * Over Heat Level						bit 32-33
+	 */
+	u_int64_t flags;
 };
 
 #endif /* DATASTRUCTS_H_ */
